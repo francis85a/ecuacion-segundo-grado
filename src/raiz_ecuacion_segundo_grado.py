c45c07b8a373efd1e85fc3c300265a89e2c6c509
francis85a <franperezcid85@gmail.com>
2025-11-20 11:28:56 +0100

    feat: :construction: Añadir funcion para discriminante menor
    añadimos una funcion para el discriminante menor a cero

--- src/raiz_ecuacion_segundo_grado.py
+++ src/raiz_ecuacion_segundo_grado.py
@@ -13,7 +13,9 @@ def raiz_ecuacion_segundo_grado(a, b, c):
         return x, y
     
     discriminante = b ** 2 - 4 * a * c
-
+    
+    if discriminante <= 0:
+        return None
     if discriminante >= 0:
         x = -b + math.sqrt(discriminante) / (2 * a)
         y = -b - math.sqrt(discriminante) / (2 * a)
